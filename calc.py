95055041627aa639fa171e4b394d334738cc2d91
IKEDA Etsuko <21716007@jwu.ac.jp>
2019-09-30 16:49:11 +0900

    broccoli-e

--- calc.py
+++ calc.py
@@ -1,6 +1,8 @@
 
 def calc(s):
-    return int(s)
+    print ('s=', s)
+    nums = map(int, s.split('+'))
+    print('nums=',nums)
+    return sum(nums)
 
-print (calc("1"))
-
+    print(clac("1+2"))
