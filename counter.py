dcd3d2c7558c17196a1b5c8c1ce7b078f40f0db6
IKEDA Etsuko <21716007@jwu.ac.jp>
2019-10-07 15:52:27 +0900

    counter.class

---
+++ counter.py
@@ -0,0 +1,25 @@
+
+class counter(object):
+    def__init__(self):
+       self.cnt = 0
+
+def count(self):
+    self.cnt +=1
+
+def doublecount(self):
+    self.cut += 2
+def reset(self):
+    self.cnt =0
+
+def show (self):
+    print(self.cnt)
+
+def __repr__ (self):
+    return str(self.cnt)
+
+c = counter()
+c.show()
+c.doublecount()
+c.show()
+print(type(c))
+print(c)
